19e1e6a2c2a2a360ff4f35b405fd152fb8edad7e
Adnanh4141 <adnanh4141@gmail.com>
2024-03-29 01:06:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div className="">App</div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+
+// npm install @mui/material @emotion/react @emotion/styled
+// npm i @chakra-ui/react
+// npm install @reduxjs/toolkit react-redux
